5bb6a88fb2f5bf56750084000982f2956b804866
Danila Ulyanov <danila@ulyanov.net>
2014-07-22 16:25:45 -0500

    Increase session expiration from 3 minutes to 1 day

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 #Rails.application.config.session_store :cookie_store, key: '_pivotal-tracker-storyboard_session'
-Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 3.minutes
+Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 1.day
